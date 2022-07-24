d9f9701301bcf2434bcee4d788679512a100eea7
Jay Crisostomo <jaycris2000@gmail.com>
2022-07-23 21:14:56 -0500

    project finished

--- src/com/mortgageCalculator/Main.java
+++ src/com/mortgageCalculator/Main.java
@@ -6,15 +6,48 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int principal;
+        double annualInterestRate;
+        byte periodInYears;
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while(true) {
+            System.out.print("Principal ($1K - $1M): ");
+            try {
+                principal = scanner.nextInt();
+                if (principal >= 1000 && principal <= 1_000_000)
+                    break;
+                System.out.println("Enter a number between $1,000 and $1,000,000");
+            } catch(Exception ex) {
+                System.out.println("Enter a number between $1,000 and $1,000,000");
+                scanner.nextLine();
+            }
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        double annualInterestRate = scanner.nextFloat();
+        while(true){
+            System.out.print("Annual Interest Rate: ");
+            try {
+                annualInterestRate = scanner.nextFloat();
+                if (annualInterestRate >= 0 && annualInterestRate <= 30)
+                    break;
+                System.out.println("Enter a number between 0 and 30");
+            } catch(Exception ex) {
+                System.out.println("Enter a number between 0 and 30");
+                scanner.nextLine();
+            }
+        }
 
-        System.out.print("Period (Years): ");
-        byte periodInYears = scanner.nextByte();
+        while(true){
+            System.out.print("Period (Years): ");
+            try {
+                periodInYears = scanner.nextByte();
+                if (periodInYears >= 1 && periodInYears <= 30)
+                    break;
+                System.out.println("Enter a number between 1 and 30");
+            } catch(Exception ex) {
+                System.out.println("Enter a number between 1 and 30");
+                scanner.nextLine();
+            }
+        }
 
         //Mortgage Formula = P [ i(1 + i)^n ] / [ (1 + i)^n – 1].
         //Here’s a breakdown of each of the variables:
@@ -25,6 +58,8 @@ public class Main {
 
         double annualInterestRateToMonthly = (annualInterestRate/100)/12;
         double mortgage = principal * (annualInterestRateToMonthly*(Math.pow(1+annualInterestRateToMonthly, periodInYears*12))) / (Math.pow(1+annualInterestRateToMonthly, periodInYears*12) - 1 );
+
+        System.out.println("Calculating Mortgage...");
 
         System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
     }
